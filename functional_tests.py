0eb5c8f6b1e6646487692fc143f2fc876babcb39
M <lmiguelvargasf@gmail.com>
2016-06-29 13:34:39 -0500

    Add test to navigate to projects

--- functional_tests.py
+++ functional_tests.py
@@ -9,6 +9,13 @@ class HomePageTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def test_projects_page_can_be_accessed_from_home_page(self):
+        self.browser.get('http://localhost:8000')
+        projects_link = self.browser.find_element_by_id('projects')
+        projects_link.click()
+        self.assertIn('Projects', self.browser.find_element_by_class_name("h1").text)
+
 
 if __name__ == "__main__":
     unittest.main(warnings='ignore')
+
