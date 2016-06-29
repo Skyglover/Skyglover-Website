918ea897fc78636a025483357070517cfd6f5001
M <lmiguelvargasf@gmail.com>
2016-06-29 12:19:20 -0500

    Modify .gitignore and remove needless files

--- home/tests.py
+++ home/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from selenium import webdriver
 
-# Create your tests here.
+
+class HomePageTest(TestCase):
+    def setUp(self):
+        self.browser = webdriver.Chrome('/home/m/chromedriver')
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_home_page(self):
+        self.browser.get('http://localhost:8000')
+        self.assertIn('To-Do', self.browser.title)
