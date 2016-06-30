8c81901c260d589a3f61755f3e27506e263146d6
M <lmiguelvargasf@gmail.com>
2016-06-30 18:14:20 -0500

    Describe test

--- functional_tests/projects/tests.py
+++ functional_tests/projects/tests.py
@@ -14,17 +14,15 @@ from helper.helper import wait_for_page_to_load_with_id_or_fail
 class ProjectsPageTest(unittest.TestCase):
     def setUp(self):
         self.browser = webdriver.Chrome('/home/m/chromedriver')
+        self.browser.get('http://localhost:8000')
+        projects_link = self.browser.find_element_by_id('projects')
+        projects_link.click()
+        wait_for_page_to_load_with_id_or_fail(self, self.browser, 'title')
 
     def tearDown(self):
         self.browser.quit()
 
     def test_projects_are_displayed(self):
-        self.browser.get('http://localhost:8000')
-        projects_link = self.browser.find_element_by_id('projects')
-        projects_link.click()
-
-        wait_for_page_to_load_with_id_or_fail(self, self.browser, 'title')
-
         h1 = self.browser.find_element_by_tag_name("h1")
         self.assertEqual('Projects', h1.text)
 
@@ -39,6 +37,10 @@ class ProjectsPageTest(unittest.TestCase):
             self.assertEqual(projects[index].name, project_name)
             self.assertEqual(projects[index].description, project_description)
 
+    def test_label_is_displayed_when_no_projects_available(self):
+        # TODO: navigate to projects
+        # TODO: I see a label that indicates that currently there are no projects
+        pass
 
 if __name__ == "__main__":
     unittest.main(warnings='ignore')
