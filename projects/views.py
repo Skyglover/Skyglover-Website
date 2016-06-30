653d2136ac6a8c9316c5a657fd8261a9d22c2363
M <lmiguelvargasf@gmail.com>
2016-06-29 21:34:59 -0500

    Display projects using database

--- projects/views.py
+++ projects/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from .models import Project
 
 
 def projects_page(request):
-    return render(request, 'projects/projects.html')
+    return render(request, 'projects/projects.html', {
+        'projects': Project.objects.all(),
+    })
