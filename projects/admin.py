a5fff275ec530fb4692ebcf9d0e8d1b42a6d57d3
M <lmiguelvargasf@gmail.com>
2016-07-01 03:05:40 -0500

    Customize how model Project is displayed for an administrator

--- projects/admin.py
+++ projects/admin.py
@@ -4,6 +4,9 @@ from .models import Project
 
 class ProjectAdmin(admin.ModelAdmin):
     list_display = ('name', 'status', 'start_date')
+    list_filter = ('status',)
+    search_fields = ('name',)
+    prepopulated_fields = {'slug': ('name',)}
 
 
 admin.site.register(Project, ProjectAdmin)
