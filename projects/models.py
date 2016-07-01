a3287cbd7f3ea8bc3ec1cb08ccc63c7484b2bb21
M <lmiguelvargasf@gmail.com>
2016-07-01 00:01:51 -0500

    Change models and apply migrations

--- projects/models.py
+++ projects/models.py
@@ -1,6 +1,12 @@
 from django.db import models
+from django.template.defaultfilters import slugify
 
 
 class Project(models.Model):
     name = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250, default='')
     description = models.CharField(max_length=500)
+
+    def save(self, force_insert=False, force_update=False, using=None, update_fields=None):
+        self.slug = slugify(self.name)
+        super().save(force_insert, force_update, using, update_fields)
