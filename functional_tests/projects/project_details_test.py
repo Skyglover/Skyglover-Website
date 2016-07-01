cd71e2f9c9461d2ee380024761bdd167aff7e910
M <lmiguelvargasf@gmail.com>
2016-07-01 01:18:25 -0500

    Add TODO for test

--- functional_tests/projects/project_details_test.py
+++ functional_tests/projects/project_details_test.py
@@ -5,18 +5,19 @@ from selenium import webdriver
 sys.path.append("./")
 os.environ["DJANGO_SETTINGS_MODULE"] = "SkygloverWebSite.settings"
 setup()
-
-
+from projects.models import Project
 
 class ProjectDetailsPageTest(unittest.TestCase):
     def setUp(self):
         self.browser = webdriver.Chrome('/home/m/chromedriver')
-        self.browser.get('http://localhost:8000')
+        self.project = Project.objects.all()[0]
+        self.browser.get('http://localhost:8000/projects/' + self.project.slug + '/')
 
     def tearDown(self):
         self.browser.quit()
 
-    def test_projects_are_displayed(self):
+    def test_project_details_are_displayed(self):
+        #TODO: title, status, start_date, long_description are displayed
         self.fail('Finish test')
 
 
