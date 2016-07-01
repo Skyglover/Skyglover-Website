2d144c8669c1f032e590e208f654807fc9792d34
M <lmiguelvargasf@gmail.com>
2016-06-30 20:17:13 -0500

    Display label when no projects available

--- functional_tests/projects/tests.py
+++ functional_tests/projects/tests.py
@@ -13,17 +13,13 @@ class ProjectsPageTest(unittest.TestCase):
     def setUp(self):
         self.browser = webdriver.Chrome('/home/m/chromedriver')
         self.browser.get('http://localhost:8000')
-        projects_link = self.browser.find_element_by_id('projects')
-        projects_link.click()
-        wait_for_page_to_load_with_id_or_fail(self, self.browser, 'title')
 
     def tearDown(self):
         self.browser.quit()
 
     def test_projects_are_displayed(self):
-        h1 = self.browser.find_element_by_tag_name("h1")
-        self.assertEqual('Projects', h1.text)
-
+        self.navigate_to_projects()
+        self.verify_page_title_is_displayed()
         projects = Project.objects.all()
         displayed_projects = self.browser.find_elements_by_class_name('project')
 
@@ -36,9 +32,26 @@ class ProjectsPageTest(unittest.TestCase):
             self.assertEqual(projects[index].description, project_description)
 
     def test_label_is_displayed_when_no_projects_available(self):
-        # TODO: navigate to projects
-        # TODO: I see a label that indicates that currently there are no projects
-        self.fail("Complete tests")
+        projects = Project.objects.all()
+        projects_copy = list(projects)
+        projects.delete()
+        self.navigate_to_projects()
+        try:
+            label = self.browser.find_element_by_id('no_projects_label')
+            self.assertEquals('Currently there are no projects to show.', label.text)
+        finally:
+            for project in projects_copy:
+                project.save()
+
+    def navigate_to_projects(self):
+        projects_link = self.browser.find_element_by_id('projects')
+        projects_link.click()
+        wait_for_page_to_load_with_id_or_fail(self, self.browser, 'title')
+
+    def verify_page_title_is_displayed(self):
+        h1 = self.browser.find_element_by_tag_name("h1")
+        self.assertEqual('Projects', h1.text)
+
 
 if __name__ == "__main__":
     unittest.main(warnings='ignore')
