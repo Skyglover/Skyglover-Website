fff6de8979f6729876cd4895b9481ec6da6c4eb6
M <lmiguelvargasf@gmail.com>
2016-07-01 01:11:52 -0500

    Start tests on projects page

--- functional_tests/projects/projects_page_test.py
+++ functional_tests/projects/projects_page_test.py
@@ -13,13 +13,15 @@ from helper.helper import verify_page_h1_is_displayed
 class ProjectsPageTest(unittest.TestCase):
     def setUp(self):
         self.browser = webdriver.Chrome('/home/m/chromedriver')
-        self.browser.get('http://localhost:8000')
+        self.go_to_projects()
+
+    def go_to_projects(self):
+        self.browser.get('http://localhost:8000/projects/')
 
     def tearDown(self):
         self.browser.quit()
 
     def test_projects_are_displayed(self):
-        self.navigate_to_projects()
         verify_page_h1_is_displayed(self, self.browser, 'Projects')
         self.verify_projects_are_displayed()
 
@@ -28,7 +30,8 @@ class ProjectsPageTest(unittest.TestCase):
         projects_copy = list(projects)
         projects.delete()
 
-        self.navigate_to_projects()
+        self.go_to_projects()
+
         label = self.browser.find_element_by_id('no_projects_label')
         self.assertEquals('Currently there are no projects to show.', label.text)
 
@@ -36,7 +39,6 @@ class ProjectsPageTest(unittest.TestCase):
             project.save()
 
     def test_project_details_page_can_be_accessed_from_home_page(self):
-        self.navigate_to_projects()
         first_displayed_project = self.browser.find_elements_by_tag_name('a')[0]
         first_displayed_project.click()
         wait_for_page_to_load_with_id_or_fail(self, self.browser, 'project_details_title')
