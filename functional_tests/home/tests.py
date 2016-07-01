06be186e04e45304d17cb96b741a77fe588fc9fe
M <lmiguelvargasf@gmail.com>
2016-06-30 21:38:50 -0500

    Use method from helper

--- functional_tests/home/tests.py
+++ functional_tests/home/tests.py
@@ -2,7 +2,7 @@ import sys, unittest
 from selenium import webdriver
 sys.path.append("./")
 from helper.helper import wait_for_page_to_load_with_id_or_fail
-
+from helper.helper import verify_page_h1_is_displayed
 
 class HomePageTest(unittest.TestCase):
     def setUp(self):
@@ -17,8 +17,7 @@ class HomePageTest(unittest.TestCase):
         projects_link.click()
 
         wait_for_page_to_load_with_id_or_fail(self, self.browser, 'projects_page_title')
-        h1 = self.browser.find_element_by_tag_name("h1")
-        self.assertEqual('Projects', h1.text)
+        verify_page_h1_is_displayed(self, self.browser, 'Projects')
 
 
 if __name__ == "__main__":
