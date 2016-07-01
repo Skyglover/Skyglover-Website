c7b72325ea803e6ed2bcd4c300f3c392c1f1028a
M <lmiguelvargasf@gmail.com>
2016-07-01 02:35:21 -0500

    Discard changes in database after running tests

--- run_tests.sh
+++ run_tests.sh
@@ -2,3 +2,4 @@
 # Change /home/m/.virtualenvs/env/bin/python with the location of your python environment
 /home/m/.virtualenvs/env/bin/python manage.py test
 for test_file in ./functional_tests/*/*.py; do /home/m/.virtualenvs/env/bin/python "$test_file"; done
+git co db.sqlite3
