36c1521c782f5c393901b5606c1d7e6f2b47b9f7
M <lmiguelvargasf@gmail.com>
2016-07-03 13:46:43 -0500

    Display about information on about page

--- home/models.py
+++ home/models.py
@@ -1,3 +1,24 @@
-from django.db import models
+import unittest
+
+import sys
+from selenium import webdriver
+
+sys.path.append("./")
+from helper.helper import verify_page_h1_is_displayed
 
 
+class AboutPageTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Chrome('/home/m/chromedriver')
+        self.browser.get('http://localhost:8000/about/')
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_about_page_elements_are_displayed(self):
+        verify_page_h1_is_displayed(self, self.browser, 'About')
+        about_information = self.browser.find_element_by_tag_name('p')
+        self.assertEqual('We are ...', about_information.text)
+
+if __name__ == "__main__":
+    unittest.main(warnings='ignore')
