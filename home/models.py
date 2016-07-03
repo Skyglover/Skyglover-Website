e5201569360918b0862fa032fce5141e33228dee
M <lmiguelvargasf@gmail.com>
2016-07-03 14:06:26 -0500

    Make migrations

--- home/models.py
+++ home/models.py
@@ -1,24 +1,9 @@
-import unittest
-
-import sys
-from selenium import webdriver
-
-sys.path.append("./")
-from helper.helper import verify_page_h1_is_displayed
+from django.db import models
 
 
-class AboutPageTest(unittest.TestCase):
-    def setUp(self):
-        self.browser = webdriver.Chrome('/home/m/chromedriver')
-        self.browser.get('http://localhost:8000/about/')
+class SomeText(models.Model):
+    identifier = models.CharField(max_length=100, db_index=True)
+    text = models.TextField()
 
-    def tearDown(self):
-        self.browser.quit()
-
-    def test_about_page_elements_are_displayed(self):
-        verify_page_h1_is_displayed(self, self.browser, 'About')
-        about_information = self.browser.find_element_by_tag_name('p')
-        self.assertEqual('We are ...', about_information.text)
-
-if __name__ == "__main__":
-    unittest.main(warnings='ignore')
+    class Meta:
+        ordering = ('identifier',)
