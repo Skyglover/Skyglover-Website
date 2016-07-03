c28fafec788e05bb6f5426e69e9300e07dcf3e3c
M <lmiguelvargasf@gmail.com>
2016-07-03 11:20:35 -0500

    Create team page

--- SkygloverWebSite/urls.py
+++ SkygloverWebSite/urls.py
@@ -18,11 +18,13 @@ from django.contrib import admin
 from home.views import home_page
 from projects.views import projects_page
 from projects.views import project_details_page
+from team.views import team_page
 
 
 urlpatterns = [
     url(r'^$', home_page, name='home'),
     url(r'^projects/$', projects_page, name='projects'),
     url(r'^projects/(?P<project_name_slug>[-\w]+)/$', project_details_page, name='project_details'),
+    url(r'^team/$', team_page, name='team'),
     url(r'^admin/', include(admin.site.urls)),
 ]
--- team/views.py
+++ team/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def team_page(request):
+    return render(request, 'team/team.html')
