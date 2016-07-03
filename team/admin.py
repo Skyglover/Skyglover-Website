05bea6c3061ec63bbedf664d31ba071dc20ae820
M <lmiguelvargasf@gmail.com>
2016-07-03 12:01:38 -0500

    Reformat file

--- team/admin.py
+++ team/admin.py
@@ -2,7 +2,9 @@ from django.contrib import admin
 
 from team.models import Member
 
+
 class MemberAdmin(admin.ModelAdmin):
     list_display = ('name', 'position')
+
 
 admin.site.register(Member, MemberAdmin)
