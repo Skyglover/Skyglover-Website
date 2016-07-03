2987789b6671d2547a7a470f7329c9bd049e2e5e
M <lmiguelvargasf@gmail.com>
2016-07-03 12:00:49 -0500

    Sort members by id

--- team/models.py
+++ team/models.py
@@ -5,3 +5,6 @@ class Member(models.Model):
     name = models.CharField(max_length=200)
     position = models.CharField(max_length=100)
     information = models.TextField()
+
+    class Meta:
+        ordering = ('id',)
