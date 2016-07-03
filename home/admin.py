4c92540f7c0a047100bc6a1daf3a23a2e7df8dab
M <lmiguelvargasf@gmail.com>
2016-07-03 14:09:59 -0500

    Customize SomeText model in admin site

--- home/admin.py
+++ home/admin.py
@@ -2,4 +2,9 @@ from django.contrib import admin
 
 from home.models import SomeText
 
+
+class SomeTextAdmin(admin.ModelAdmin):
+    list_display = ('identifier', 'text')
+
+
 admin.site.register(SomeText)
