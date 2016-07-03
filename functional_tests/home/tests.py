b454c2af44e2d3d97286a6d16f125932a73684f6
M <lmiguelvargasf@gmail.com>
2016-07-03 11:03:01 -0500

    Add test to navigate to teamm page

--- functional_tests/home/tests.py
+++ functional_tests/home/tests.py
@@ -14,17 +14,22 @@ class HomePageTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
-    def test_projects_page_can_be_accessed_from_home_page(self):
-        projects_link = self.browser.find_element_by_id('projects')
-        projects_link.click()
-
-        wait_for_page_to_load_with_id_or_fail(self, self.browser, 'projects_page_title')
-        verify_page_h1_is_displayed(self, self.browser, 'Projects')
-
     def test_home_page_elements_are_displayed(self):
         self.verify_navigation_bar_elements_are_displayed()
         self.verify_skyglover_name_and_description_are_displayed()
         self.verify_get_in_touch_is_displayed()
+
+    def test_projects_page_can_be_accessed_from_home_page(self):
+        projects_link = self.browser.find_element_by_id('projects')
+        projects_link.click()
+        wait_for_page_to_load_with_id_or_fail(self, self.browser, 'projects_page_title')
+        verify_page_h1_is_displayed(self, self.browser, 'Projects')
+
+    def test_team_page_can_be_accessed_from_home_page(self):
+        projects_link = self.browser.find_element_by_id('team')
+        projects_link.click()
+        wait_for_page_to_load_with_id_or_fail(self, self.browser, 'team_page_title')
+        verify_page_h1_is_displayed(self, self.browser, 'Team')
 
     def verify_get_in_touch_is_displayed(self):
         get_in_touch = self.browser.find_element_by_id('get_in_touch')
