03e17c0edd8153870bdb19827cfb5aab47d209d3
M <lmiguelvargasf@gmail.com>
2016-07-10 15:41:15 -0500

    Remove file

--- run_tests.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-# Change /home/m/.virtualenvs/env/bin/python with the location of your python environment
-/home/m/.virtualenvs/env/bin/python manage.py test
-for test_file in ./functional_tests/*/*.py; do /home/m/.virtualenvs/env/bin/python "$test_file"; done
-git co db.sqlite3
