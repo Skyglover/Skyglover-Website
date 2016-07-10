a9dd17a52c8bbeba48b0370f970a1f6a3f0e98f4
M <lmiguelvargasf@gmail.com>
2016-07-10 15:11:15 -0500

    Improve tests using LiveServerTestCase

--- functional_tests/projects/projects_page_test.py
+++ functional_tests/projects/projects_page_test.py
@@ -9,7 +9,6 @@ from helper.helper import verify_page_h1_is_displayed
 class ProjectsPageTest(LiveServerTestCase):
     def setUp(self):
         self.browser = webdriver.Chrome()
-        self.go_to_projects()
 
     def go_to_projects(self):
         self.browser.get(self.live_server_url + '/projects/')
@@ -18,24 +17,23 @@ class ProjectsPageTest(LiveServerTestCase):
         self.browser.quit()
 
     def test_projects_are_displayed(self):
+        Project.objects.create(name='project1', summary='This is a fucking summary')
+        Project.objects.create(name='project2', summary='This is a fucking summary')
+        self.go_to_projects()
         self.verify_projects_title_is_displayed()
         self.verify_projects_are_displayed()
+        Project.objects.all().delete()
 
     def test_label_is_displayed_when_no_projects_available(self):
-        self.verify_projects_title_is_displayed()
-        projects = Project.objects.all()
-        projects_copy = list(projects)
-        projects.delete()
-
         self.go_to_projects()
+        self.verify_projects_title_is_displayed()
 
         label = self.browser.find_element_by_id('no_projects_label')
         self.assertEquals('Currently there are no projects to show.', label.text)
 
-        for project in projects_copy:
-            project.save()
-
     def test_project_details_page_can_be_accessed_from_home_page(self):
+        Project.objects.create(name='project1', summary='This is a fucking summary')
+        self.go_to_projects()
         first_displayed_project = self.browser.find_elements_by_tag_name('a')[0]
         first_displayed_project.click()
         wait_for_page_to_load_with_id_or_fail(self, self.browser, 'name')
