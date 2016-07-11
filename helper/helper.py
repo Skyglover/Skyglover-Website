60e35527073285303264248bd8969183ebb0fc76
M <lmiguelvargasf@gmail.com>
2016-07-10 19:49:40 -0500

    Move function to helper.py

--- helper/helper.py
+++ helper/helper.py
@@ -14,3 +14,8 @@ def wait_for_page_to_load_with_id_or_fail(test_case, web_driver, page_id, time_t
 
 def verify_page_h1_is_displayed(test_case, web_driver, title):
     test_case.assertEqual(title, web_driver.find_element_by_tag_name('h1').text)
+
+
+def assert_path_uses_template(test_case, path, template):
+    response = test_case.client.get(path)
+    test_case.assertTemplateUsed(response, template)
--- home/tests.py
+++ home/tests.py
@@ -1,6 +1,7 @@
 from django.core.urlresolvers import resolve
 from django.test import TestCase
 
+from helper.helper import assert_path_uses_template
 from home.models import SomeText
 from home.views import home_page
 
@@ -11,16 +12,12 @@ class HomePageTest(TestCase):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
 
-    def assertPathUsesTemplate(self, path, template):
-        response = self.client.get(path)
-        self.assertTemplateUsed(response, template)
-
     def test_home_page_uses_home_template(self):
-        self.assertPathUsesTemplate('/', 'static_pages/home.html')
+        assert_path_uses_template(self, '/', 'static_pages/home.html')
 
     def test_about_page_uses_about_template(self):
         SomeText.objects.create(identifier='about_info', text='Some random text')
-        self.assertPathUsesTemplate('/about/', 'static_pages/about.html')
+        assert_path_uses_template(self, '/about/', 'static_pages/about.html')
 
     def test_get_in_touch_page_uses_get_in_touch_template(self):
-        self.assertPathUsesTemplate('/get-in-touch/', 'static_pages/get_in_touch.html')
+        assert_path_uses_template(self, '/get-in-touch/', 'static_pages/get_in_touch.html')
