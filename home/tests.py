22eed6a9b6b1356e1559c3c8667f404b60cdb03d
M <lmiguelvargasf@gmail.com>
2016-07-10 19:24:07 -0500

    Improve tests that check templates are used correctly

--- home/tests.py
+++ home/tests.py
@@ -1,12 +1,8 @@
 from django.core.urlresolvers import resolve
-from django.http import HttpRequest
-from django.template.loader import render_to_string
 from django.test import TestCase
 
 from home.models import SomeText
 from home.views import home_page
-from home.views import about_page
-from home.views import get_in_touch_page
 
 
 class HomePageTest(TestCase):
@@ -16,34 +12,14 @@ class HomePageTest(TestCase):
         self.assertEqual(found.func, home_page)
 
     def test_home_page_uses_home_template(self):
-        request = HttpRequest()
-        response = home_page(request)
-        expected_content = render_to_string('static_pages/home.html')
-
-        self.assertEqual(
-            expected_content,
-            response.content.decode()
-        )
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'static_pages/home.html')
 
     def test_about_page_uses_about_template(self):
         SomeText.objects.create(identifier='about_info', text='Some random text')
-        request = HttpRequest()
-        response = about_page(request)
-        expected_content = render_to_string('static_pages/about.html', {
-            'about_information': SomeText.objects.get(identifier='about_info').text,
-        })
-
-        self.assertEqual(
-            expected_content,
-            response.content.decode()
-        )
+        response = self.client.get('/about/')
+        self.assertTemplateUsed(response, 'static_pages/about.html')
 
     def test_get_in_touch_page_uses_get_in_touch_template(self):
-        request = HttpRequest()
-        response = get_in_touch_page(request)
-        expected_content = render_to_string('static_pages/get_in_touch.html')
-
-        self.assertEqual(
-            expected_content,
-            response.content.decode()
-        )
+        response = self.client.get('/get-in-touch/')
+        self.assertTemplateUsed(response, 'static_pages/get_in_touch.html')
